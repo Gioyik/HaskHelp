be6acffbb6e4bda2660632c04c4106417558a368
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-02-26 14:15:00 -0500

    added lib

---
+++ HaskHelp.hs
@@ -0,0 +1,17 @@
+module HaskHelp where
+
+import System.Process
+import Control.Concurrent
+import System.Random
+
+say x = do
+  readProcessWithExitCode "say" [x] []
+  return ()
+p = putStrLn
+i = getLine
+w delay = threadDelay $ delay * 1000000
+s xs = sequence xs
+m a = mapM_ a
+rnd n = randomIO >>= return . (+1) . (`mod` n)
+int :: String -> Int
+int str = read str
